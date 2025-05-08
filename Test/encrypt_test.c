88f2bb2457858b71649493eac9ebd2fd6f6e8f6d
Zk47T <zizuzacker@gmail.com>
2025-05-08 18:20:25 +0700

    Update

--- Test/encrypt_test.c
+++ Test/encrypt_test.c
@@ -11,29 +11,37 @@
 int main(void) {
     const char *msg = "Hello, Kernel!";
     size_t len = strlen(msg)+1;
-    char buf[1024] = {0};
+    char buf[1024] = {0 };
     int mode, fd;
 
-    // --- ENCRYPT ---
     fd = open(DEVICE_PATH, O_RDWR);
-    if (fd < 0) { perror("open"); return EXIT_FAILURE; }
+    if (fd < 0) 
+    { 
+        perror("open"); return EXIT_FAILURE; 
+    }
 
     mode = MODE_ENCRYPT;
-    if (ioctl(fd, IOCTL_SET_MODE, &mode) < 0) { perror("ioctl"); close(fd); return 1; }
+    if (ioctl(fd, IOCTL_SET_MODE, &mode) < 0) 
+    { 
+        perror("ioctl"); close(fd); return 1; 
+    }
     write(fd, msg, len);
     read(fd, buf, len);
-    printf("Encrypted: %s\n", buf);
+    printf("Encrypted result: %s\n", buf);
     close(fd);
 
-    // --- DECRYPT ---
+
     fd = open(DEVICE_PATH, O_RDWR);
-    if (fd < 0) { perror("open"); return EXIT_FAILURE; }
+    if (fd < 0) 
+    { 
+        perror("open"); return EXIT_FAILURE; 
+    }
 
     mode = MODE_DECRYPT;
     if (ioctl(fd, IOCTL_SET_MODE, &mode) < 0) { perror("ioctl"); close(fd); return 1; }
     write(fd, buf, len);
     read(fd, buf, len);
-    printf("Decrypted: %s\n", buf);
+    printf("Decrypted result: %s\n", buf);
     close(fd);
 
     return EXIT_SUCCESS;
